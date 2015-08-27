09e5165a9884462e9ef6ec1fde22272fc08e604c
nathanderaymond <nad513@lehigh.edu>
2015-08-27 15:43:42 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+   // Nathan DeRaymond
+   // 8/27/2015
+   // CSE 2 Lab 01 -- HelloWorld
+   // This program prints out "Hello World 2015!!"
+   
+   // Defining class
+    public class HelloWorld {
+            
+        // add main method
+        public static void main(String[] args) {
+            
+            //Printing the statement with inside a box
+            System.out.println("########################");
+            System.out.println("########################");
+            System.out.println("#                      #");
+            System.out.println("#  Hello World 2015!!  #");
+            System.out.println("#                      #");
+            System.out.println("########################");
+            System.out.println("########################");
+            System.out.println("test");
+            
+            //end of program
+        }
+    }
