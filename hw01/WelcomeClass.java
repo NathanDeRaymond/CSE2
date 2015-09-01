3f5717d56ca343f813395a36ff87fe07cfcbd7db
nathanderaymond <nad513@lehigh.edu>
2015-09-01 17:18:31 +0000

    wrote new code for hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+   // Nathan DeRaymond
+   // 9/1/2015
+   // CSE 2 Homework 01 -- Welcome Class
+   // This program prints out a welcome screen and a short autobiographical statement
+   
+   // Defining class
+    public class WelcomeClass {
+            
+        // add main method
+        public static void main(String[] args) {
+            
+            //Printing the statements below to the screen
+            System.out.println("    -----------");
+            System.out.println("    | WELCOME |");
+            System.out.println("    -----------");
+            System.out.println("  ^  ^  ^  ^  ^  ^");
+            System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\"); // need two backslashes or compiler will give error for illegal escape character
+            System.out.println("|-N--A--D--5--1--3-|");
+            System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");  
+            System.out.println("  v  v  v  v  v  v");
+            System.out.println(""); // puts in a line of space
+            System.out.println("Over the past few years, I have particpated in marching activity, performing");
+            System.out.println("with the Cadets2 drum corps and the Lehigh marching band.");
+            System.out.println("");
+            
+            //end of program
+        }
+    }
